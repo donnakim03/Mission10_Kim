eb84d2c6275023d4ec9302e51bdc5b3d75ef1639
donnakim03 <dkim210@byu.edu>
2024-03-14 19:56:56 -0600

    created a heading component

---
+++ frontend/src/Heading.tsx
@@ -0,0 +1,11 @@
+function Heading() {
+  return (
+    <div>
+      <h1>
+        This page displays information about bowlers in the bowling league.
+      </h1>
+    </div>
+  );
+}
+
+export default Heading;
